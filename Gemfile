7876faed9de5bebccf8f31f03580520ea7275910
vasylluk <vasyl3luk@gmail.com>
2018-03-29 23:29:08 +0100

    tinymce add

--- Gemfile
+++ Gemfile
@@ -37,6 +37,8 @@ gem 'activeadmin'
 gem 'devise'
 #avatars
 gem "paperclip", "~> 6.0.0"
+#text edit
+gem 'tinymce-rails'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,15 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
-    member do 
-      get :chosen
-    end
-
+  
   resources :categories, only:[:index,:show]
 
   resources :questions do 
+    member do 
+      get :chosen
+  end
+
     resources :answers, only:[:create,:update,:destroy] 
-    
   end
   get "answers/:id/positiv_vote", to: "answers#positiv_vote", as:"positiv_vote"
   get "answers/:id/negativ_vote", to: "answers#negativ_vote", as:"negativ_vote"
