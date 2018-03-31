1875d2cc55e8bd854343b0944064bd282385c835
nbilinskyi <n.bilinskiy@gmail.com>
2018-03-31 13:59:33 +0300

    i18n fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :devise_sign_up_params, if: :devise_controller? 
+	before_action :set_locale
+	 
+	def set_locale
+	  I18n.locale = params[:locale] || I18n.default_locale
+	end
+
 
  private
   def devise_sign_up_params
