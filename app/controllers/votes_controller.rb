a215e05ddb4cc1182f1f89edf959a7b9ce58a5c0
Civil21 <diusolexander@gmail.com>
2018-04-30 01:55:24 +0300

    update vote and karma

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,34 +4,42 @@ class VotesController < ApplicationController
 
 	def positiv_vote
 		if current_user.id != @object.user.id
-			@vote=Vote.find_by(user_id: current_user.id,resource_type: @object.class.name,resource_id: @object.id)
-		if @vote==nil
-			@vote=Vote.create(user_id: current_user.id,resource_type: @object.class.name,resource_id: @object.id,score: 1)
-	    else	
-	    	if @vote.score == -1
-	    	    @vote.update(score: 0)
-	        else
-	        	@vote.update(score: 1)
-	        end
-	    end
-	    @object.update(score: Vote.where(resource_type: @object.class.name,resource_id: @object.id).sum(:score))
+			if @vote==nil
+				@vote=Vote.create(user_id: current_user.id,resource_type: @object.class.name,resource_id: @object.id,score: 1)
+	    	else	
+	    		@object.score-=@vote.score
+	    		@user.karma-=@vote.score
+	    		if @vote.score == -1
+	    	    	@vote.update(score: 0)
+	        	else
+	        		@vote.update(score: 1)
+	        	end
+	    	end
+	    	@object.score+=@vote.score
+	    	@object.save
+	    	@user.karma+=@vote.score
+	    	@user.save
+	    	#@object.update(score: Vote.where(resource_type: @object.class.name,resource_id: @object.id).sum(:score))
 		end
 	    redirect_back(fallback_location: root_path)
 	end
 
 	def negativ_vote
 		if current_user.id != @object.user.id
-		@vote=Vote.find_by(user_id: current_user.id,resource_type: @object.class.name,resource_id: @object.id)
-		if @vote==nil
-		    @vote=Vote.create(user_id: current_user.id,resource_type: @object.class.name,resource_id: @object.id,score: -1)
-	    else
-	    	if @vote.score == 1
-	    	    @vote.update(score: 0)
-	        else
-	        	@vote.update(score: -1)
-	        end
-	    end
-	    @object.update(score: Vote.where(resource_type: @object.class.name,resource_id: @object.id).sum(:score))
+			if @vote==nil
+		    	@vote=Vote.create(user_id: current_user.id,resource_type: @object.class.name,resource_id: @object.id,score: -1)
+		    else
+	    		@object.update(score: @object.score-@vote.score)
+	    		@user.update(karma: @user.karma-@vote.score)
+		    	if @vote.score == 1
+		    	    @vote.update(score: 0)
+	    	    else
+	        		@vote.update(score: -1)
+		        end
+		    end
+		   	@object.update(score: @object.score+@vote.score)
+		    #@object.update(score: Vote.where(resource_type: @object.class.name,resource_id: @object.id).sum(:score))
+		   	@user.update(karma: @user.karma+@vote.score)
 		end
 	    redirect_back(fallback_location: root_path)
 	end
@@ -40,6 +48,8 @@ class VotesController < ApplicationController
 
 	def get_object
 		@object=params[:type].constantize.find(params[:id])
+		@user=@object.user.userparam
+		@vote=Vote.find_by(user_id: current_user.id,resource_type: @object.class.name,resource_id: @object.id)
 	end
 
 end
