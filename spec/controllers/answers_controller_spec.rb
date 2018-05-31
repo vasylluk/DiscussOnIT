833d2150738423183c49c6aadf6f193ea586e59a
vasylluk <vasyl3luk@gmail.com>
2018-05-31 05:51:34 +0300

    pages, postcom,ans contr test and other adds

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,25 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
+let!(:user) { build(:user) }
 
-  describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
+login_user
+
+let!(:answer) { build(:answer) }
+
+  it "should have a current_user" do
+    expect(subject.current_user).to_not eq(nil)
+  end
+
+  describe "GET #show" do
+    it "has a 200 status code" do
+      expect(response.status).to eq(200)
+    end
+    it "responds to html by default" do
+      expect(response.content_type)=="text/html"
     end
   end
 
-  describe "GET #destroy" do
-    it "returns http success" do
-      get :destroy
-      expect(response).to have_http_status(:success)
+  describe 'POST #destroy' do
+    it "should not delete unavailable answer" do
+      expect(:delete => "/answers/2321").not_to be_routable
     end
   end
 
-  describe "GET #positiv_vote" do
-    it "returns http success" do
-      get :positiv_vote
-      expect(response).to have_http_status(:success)
+  describe 'POST #create' do
+    before do
+      @question = create(:question)
+      @answer = create(:answer, user_id: subject.current_user.id, question_id: @question.id, text: "TESTSERS")
+    end
+    it "should be true" do
+      # post :create, params: {answer: {text: "EWWSWESW"}}
+      expect(@answer.text).to eq("TESTSERS")
+    end
+  end
+  
+  describe 'PATCH #update' do
+    # before do
+    #   @question = create(:question)
+    #   @answer = create(:answer, user_id: subject.current_user.id, question_id: @question.id, text: "TESTSERS")
+    # end  
+    it "should update answer" do 
+      patch :update, params: { user_id: subject.current_user.id, answer: {text: "Test123321"}}
+      expect(@answer.text)=="Test123321"
     end
   end
 
--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe VotesController, type: :controller do
+let!(:user) { build(:user) }
+
+login_user
+
+let!(:vote) { build(:vote) }
+
+  describe 'POST #destroy' do
+    it "should not delete anyway" do
+      expect(:delete => "/vote/21").not_to be_routable
+    end
+  end
+
+  it "should have a current_user" do
+    expect(subject.current_user).to_not eq(nil)
+  end
+
 
 end
